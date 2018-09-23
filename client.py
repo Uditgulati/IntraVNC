7dcf9739640923a857bff4e64a9cb464a9daf473
uditgulati <uditgulati0@gmail.com>
2018-09-23 16:27:53 +0530

    Image sending

--- client.py
+++ client.py
@@ -1,22 +1,54 @@
-# client.py  
 import socket
-
-# create a socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-
-# get local machine name
-host = socket.gethostname()                           
-
-port = 9999
-
-# connection to hostname on the port.
-s.connect((host, port))                               
-
-# Receive no more than 1024 bytes
-tm = s.recv(1024)                                     
+import cv2
+import numpy as np
+import pyscreenshot
+import imutils
+from cStringIO import StringIO
 
 
 
-print("The time got from the server is %s" % tm.decode('ascii'))
+def recieveNumpy(c):
+
+		length = None
+		ultimate_buffer = ""
+		while True:
+			data = c.recv(1024)
+			ultimate_buffer += data
+			if len(ultimate_buffer) == length:
+				break
+			while True:
+				if length is None:
+					if ':' not in ultimate_buffer:
+					    break
+					# remove the length bytes from the front of ultimate_buffer
+					# leave any remaining bytes in the ultimate_buffer!
+					length_str, ignored, ultimate_buffer = ultimate_buffer.partition(':')
+					length = int(length_str)
+				if len(ultimate_buffer) < length:
+					break
+				# split off the full message from the remaining bytes
+				# leave any remaining bytes in the ultimate_buffer!
+				ultimate_buffer = ultimate_buffer[length:]
+				length = None
+				break
+		final_image = np.load(StringIO(ultimate_buffer))['frame']
+		print 'frame received'
+		return final_image
 
 
+if __name__ == '__main__':
+	host = '127.0.0.1'
+	port = 5001
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((host, port))
+
+	message = raw_input("-> ")
+	while(message.lower() != 'quit'):
+		s.send(message)
+		frame = recieveNumpy(s)
+		print("Recieved from server: ")
+		cv2.imshow('Frame', frame)
+		if cv2.waitKey(25) & 0xFF == ord('q'):
+			break
+	s.close()
--- server.py
+++ server.py
@@ -1,27 +1,52 @@
-# server.py 
-import socket                                         
-import time
+import socket
+import cv2
+from scipy.misc import imresize
+import numpy as np
+import pyscreenshot
+import imutils
+from cStringIO import StringIO
 
-# create a socket object
-serversocket = socket.socket(
-	        socket.AF_INET, socket.SOCK_STREAM) 
 
-# get local machine name
-host = socket.gethostname()                           
 
-port = 9999                                           
+def sendNumpy(c, image):
+		if not isinstance(image, np.ndarray):
+			print 'not a valid numpy image'
+			return
+		f = StringIO()
+		np.savez_compressed(f, frame=image)
+		f.seek(0)
+		out = f.read()
+		val = "{0}:".format(len(f.getvalue()))  # prepend length of array
+		out = val + out
+		try:
+			c.sendall(out)
+		except Exception:
+			exit()
+		print 'image sent'
 
-# bind to the port
-serversocket.bind((host, port))                                  
 
-# queue up to 5 requests
-serversocket.listen(5)                                           
+if __name__ == '__main__':
+	host = ''
+	port = 5001
 
-while True:
-    # establish a connection
-    clientsocket,addr = serversocket.accept()      
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind((host, port))
 
-    print("Got a connection from %s" % str(addr))
-    currentTime = time.ctime(time.time()) + "\r\n"
-    clientsocket.send(currentTime.encode('ascii'))
-    clientsocket.close()
+	s.listen(1)
+	print("Now listening: ")
+	c, addr = s.accept()
+	print("Connection recieved: " + str(addr))
+
+	cap = cv2.VideoCapture(0)
+
+	while True:
+		data = c.recv(1024)
+		if not data:
+			break
+		print("From Connected user: " + str(data))
+		image = pyscreenshot.grab()
+		image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+		frame_resize = imresize(image, .5)
+		print("Sending: ")
+		sendNumpy(c, frame_resize)
+	c.close()
